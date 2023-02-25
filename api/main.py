c622e70603587dc9e575ce5983774a4fc721f138
amixedcolor <amixedcolor@gmail.com>
2023-02-25 16:45:55 +0900

    デプロイしたフロントエンドに対してCORS問題を解決

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
 
+origins = [
+    "https://soonawatchy.netlify.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload")
 async def uploadVideo(video: UploadFile = File(...)):
